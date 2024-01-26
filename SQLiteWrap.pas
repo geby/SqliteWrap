9fe2b9d7748a3823640777642c15c6f56cb6cf71
Lukas Gebauer <gebauerl@gmail.com>
2024-01-26 16:15:46 +0100

    2.1.0

--- SQLiteWrap.pas
+++ SQLiteWrap.pas
@@ -6,6 +6,11 @@ Note: Requires Sqlite 3.6.8 and higher!
 
 Designed for Delphi 6+ and Freepascal, Unicode support for Delphi 2009+
 
+  V2.1.0
+    Added BLOB parameters and improved BLOB handling.
+    Added TotalChanges property.
+    Some bugfixes.
+
   V2.0.0  29 June 2010
     Ported to D2009 Unicode by Roger Lascelles (support@veecad.com)
 
@@ -150,6 +155,8 @@ type
     function LastInsertRowID: int64;
     {: Return number of modified rows by last query.}
     function LastChangedRows: int64;
+    {: Return number of modified rows starting by opened database connection.}
+    function TotalChanges: int64;
     {: Set wait timeout. if database is locked, then it wait this timeout.
        If database is not released within this timeout, then error is returned.}
     procedure SetTimeout(Value: integer);
@@ -169,9 +176,14 @@ type
     procedure AddParamText(const name: String; const value: String);
     {: Add named query parameter with null value.}
     procedure AddParamNull(const name: String);
+    {: Add named query parameter of BLOB type from the memory buffer.}
+    procedure AddParamBlobPtr(const name: String; buffer: pointer; len: integer);
+    {: Add named query parameter of BLOB type from the binary string.}
+    procedure AddParamBlobText(const name: String; const value: AnsiString);
+    {: Add named query parameter of BLOB type from the stream.}
+    procedure AddParamBlob(const name: String; const value: TStream; len: integer);
     {: SQLite database handler.}
     property DB: TSQLiteDB read fDB;
-  published
     {: Debug hook for log all called queries.}
     property OnQuery: THookQuery read FOnQuery write FOnQuery;
   end;
@@ -224,6 +236,12 @@ type
     procedure AddParamText(const name: String; const value: String);
     {: Add named query parameter with null value.}
     procedure AddParamNull(const name: String);
+    {: Add named query parameter of BLOB type from memory buffer.}
+    procedure AddParamBlobPtr(const name: String; buffer: pointer; len: integer);
+    {: Add named query parameter of BLOB type from binary string.}
+    procedure AddParamBlobText(const name: String; const value: AnsiString);
+    {: Add named query parameter of BLOB type from stream.}
+    procedure AddParamBlob(const name: String; const value: TStream; len: integer);
     {: Return value of some field in current row.}
     property Fields[I: cardinal]: String read GetFields;
     {: Return value of named field in current row.}
@@ -232,7 +250,6 @@ type
     property FieldIndex[FieldName: String]: integer read GetFieldIndex;
     {: Return field type of some field.}
     property Columns[I: integer]: String read GetColumns;
-  published
     {: Indicate last row in result set.}
     property EOF: boolean read FEOF;
     {: Return number of fields in row.}
@@ -278,7 +295,7 @@ begin
   fDb := nil;
   try
     {$IFDEF SQUNI}
-    iResult := SQLite3_Open(@UTF8String(FileName)[1], Fdb);
+    iResult := SQLite3_Open(PAnsiChar(UTF8String(FileName)), Fdb);
     {$ELSE}
     iResult := SQLite3_Open(PAnsiChar(AnsiToUtf8(FileName)), Fdb);
     {$ENDIF}
@@ -317,6 +334,11 @@ begin
   Result := SQLite3_Changes(self.fDB);
 end;
 
+function TSQLiteDatabase.TotalChanges: int64;
+begin
+  Result := SQLite3_TotalChanges(self.fDB);
+end;
+
 procedure TSQLiteDatabase.RaiseError(const s, SQL: String);
 var
   Msg: PAnsiChar;
@@ -339,7 +361,7 @@ var
   iStepResult: integer;
 begin
   try
-    if Sqlite3_Prepare_v2(self.fDB, @UTF8String(SQl)[1], -1, Stmt, NextSQLStatement) <>
+    if Sqlite3_Prepare_v2(self.fDB, PAnsiChar(UTF8String(SQL)), -1, Stmt, NextSQLStatement) <>
       SQLITE_OK then
       RaiseError(c_errorsql, SQL);
     if (Stmt = nil) then
@@ -454,7 +476,7 @@ end;
 procedure TSQLiteDatabase.AddCustomCollate(name: String;
   xCompare: TCollateXCompare);
 begin
-  sqlite3_create_collation(fdb, @UTF8String(name)[1], SQLITE_UTF8, nil, xCompare);
+  sqlite3_create_collation(fdb, PAnsiChar(UTF8String(name)), SQLITE_UTF8, nil, xCompare);
 end;
 
 procedure TSQLiteDatabase.AddSystemCollate;
@@ -516,6 +538,37 @@ begin
   fParams.Add(par);
 end;
 
+procedure TSQLiteDatabase.AddParamBlobPtr(const name: String; buffer: pointer;
+  len: integer);
+var
+  par: TSQliteParam;
+begin
+  par := TSQliteParam.Create;
+  par.name := UTF8String( name );
+  par.valuetype := SQLITE_BLOB;
+  setlength(par.valuedata, len);
+  if len > 0 then
+    move(buffer^, pointer(par.valuedata)^, len);
+  fParams.Add(par);
+end;
+
+procedure TSQLiteDatabase.AddParamBlobText(const name: String; const value: AnsiString);
+begin
+  AddParamBlobPtr(name, PAnsiChar(value), length(value));
+end;
+
+procedure TSQLiteDatabase.AddParamBlob(const name: String;
+  const value: TStream; len: integer);
+var
+  buffer: AnsiString;
+  x: integer;
+begin
+  setlength(buffer, len);
+  x := value.Read(pointer(buffer[1])^, len);
+  setlength(buffer, x);
+  AddParamBlobText(name, buffer);
+end;
+
 procedure TSQLiteDatabase.SetParams(const Stmt: TSQLiteStmt);
 var
   n: integer;
@@ -526,6 +579,8 @@ begin
     for n := 0 to fParams.Count - 1 do
     begin
       par := TSQliteParam(fParams[n]);
+      if par.name = '' then
+        continue;
       i := sqlite3_bind_parameter_index(Stmt, @par.name[1]);
       if i > 0 then
       begin
@@ -535,8 +590,19 @@ begin
           SQLITE_FLOAT:
             sqlite3_bind_double(Stmt, i, par.valuefloat);
           SQLITE_TEXT:
-            sqlite3_bind_text(Stmt, i, @par.valuedata[1],
-              length(par.valuedata), SQLITE_TRANSIENT);
+            if par.valuedata = '' then
+              sqlite3_bind_text(Stmt, i, PAnsiChar(par.valuedata),
+                0, SQLITE_TRANSIENT)
+            else
+              sqlite3_bind_text(Stmt, i, @par.valuedata[1],
+                length(par.valuedata), SQLITE_TRANSIENT);
+          SQLITE_BLOB:
+            if par.valuedata = '' then
+              sqlite3_bind_blob(Stmt, i, PAnsiChar(par.valuedata),
+                0, SQLITE_TRANSIENT)
+            else
+              sqlite3_bind_blob(Stmt, i, @par.valuedata[1],
+                length(par.valuedata), SQLITE_TRANSIENT);
           SQLITE_NULL:
             sqlite3_bind_null(Stmt, i);
         end;
@@ -555,7 +621,7 @@ end;
 
 function TSQLiteDatabase.InTransaction: Boolean;
 begin
-  Result := SQLite3_Get_Autocommit(FDB) = 0; 
+  Result := SQLite3_Get_Autocommit(FDB) = 0;
 end;
 
 { TSQLiteTable }
@@ -564,6 +630,8 @@ procedure TSQLiteTable.AddParamFloat(const name: String; value: double);
 var
   i: integer;
 begin
+  if name = '' then
+    exit;
   i := sqlite3_bind_parameter_index(FStmt, @UTF8String(name)[1]);
   if i > 0 then
     sqlite3_bind_double(FStmt, i, value);
@@ -573,6 +641,8 @@ procedure TSQLiteTable.AddParamInt(const name: String; value: int64);
 var
   i: integer;
 begin
+  if name = '' then
+    exit;
   i := sqlite3_bind_parameter_index(FStmt, @UTF8String(name)[1]);
   if i > 0 then
     sqlite3_bind_int64(FStmt, i, value);
@@ -582,6 +652,8 @@ procedure TSQLiteTable.AddParamNull(const name: String);
 var
   i: integer;
 begin
+  if name = '' then
+    exit;
   i := sqlite3_bind_parameter_index(FStmt, @UTF8String(name)[1]);
   if i > 0 then
     sqlite3_bind_null(FStmt, i);
@@ -592,12 +664,49 @@ var
   i: integer;
   valueUTF8 : UTF8String;
 begin
+  if name = '' then
+    exit;
   i := sqlite3_bind_parameter_index(FStmt, @UTF8String(name)[1] );
   if i > 0 then
   begin
     valueUTF8 := UTF8String( value );
-    sqlite3_bind_text(FStmt, i, @valueUTF8[1], length(valueUTF8), SQLITE_TRANSIENT);
+    if valueUTF8 = '' then
+      sqlite3_bind_text(FStmt, i, pansichar(valueUTF8), 0, SQLITE_TRANSIENT)
+    else
+      sqlite3_bind_text(FStmt, i, @valueUTF8[1], length(valueUTF8), SQLITE_TRANSIENT);
   end;
+end;
+
+procedure TSQLiteTable.AddParamBlobPtr(const name: String; buffer: pointer; len: integer);
+var
+  i: integer;
+begin
+  if name = '' then
+    exit;
+  i := sqlite3_bind_parameter_index(FStmt, @UTF8String(name)[1]);
+  if i > 0 then
+    sqlite3_bind_blob(FStmt, i, buffer, len, SQLITE_TRANSIENT);
+end;
+
+procedure TSQLiteTable.AddParamBlobText(const name: String; const value: AnsiString);
+begin
+  if name = '' then
+    exit;
+  AddParamBlobPtr(name, PAnsiChar(value), length(value));
+end;
+
+procedure TSQLiteTable.AddParamBlob(const name: String;
+  const value: TStream; len: integer);
+var
+  buffer: AnsiString;
+  x: integer;
+begin
+  if name = '' then
+    exit;
+  setlength(buffer, len);
+  x := value.Read(pointer(buffer[1])^, len);
+  setlength(buffer, x);
+  AddParamBlobText(name, buffer);
 end;
 
 constructor TSQLiteTable.Create(const DB: TSQLiteDatabase; const SQL: String; PrepareOnly: Boolean = false);
@@ -611,7 +720,7 @@ begin
   self.fRow := 0;
   self.fColCount := 0;
   self.fSQL := SQL;
-  if Sqlite3_Prepare_v2(DB.fDB, @UTF8String(SQL)[1], -1, fStmt, NextSQLStatement) <> SQLITE_OK then
+  if Sqlite3_Prepare_v2(DB.fDB, PAnsiChar(UTF8String(SQL)), -1, fStmt, NextSQLStatement) <> SQLITE_OK then
     DB.RaiseError(c_errorsql, SQL);
   if (fStmt = nil) then
     DB.RaiseError(c_errorprepare, SQL);
@@ -671,15 +780,17 @@ begin
       if MemStream.Size > 0 then
       begin
         MemStream.position := 0;
-        {$IFDEF UNICODE}
-        Buffer := AnsiStralloc(MemStream.Size + 1);
-        {$ELSE}
-        Buffer := Stralloc(MemStream.Size + 1);
-        {$ENDIF}
-        MemStream.readbuffer(Buffer[0], MemStream.Size);
-        (Buffer + MemStream.Size)^ := chr(0);
+        Buffer := MemStream.Memory;
         SetString(Result, Buffer, MemStream.size);
-        strdispose(Buffer);
+//        {$IFDEF UNICODE}
+//        Buffer := AnsiStralloc(MemStream.Size + 1);
+//        {$ELSE}
+//        Buffer := Stralloc(MemStream.Size + 1);
+//        {$ENDIF}
+//        MemStream.readbuffer(Buffer[0], MemStream.Size);
+//        (Buffer + MemStream.Size)^ := chr(0);
+//        SetString(Result, Buffer, MemStream.size);
+//        strdispose(Buffer);
       end;
      finally
      MemStream.Free;
